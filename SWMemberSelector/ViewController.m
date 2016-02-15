7cac41ec1aaf7374643ddd8c98c5ac9f4073b818
MareWave <marewave@gmail.com>
2016-02-15 16:09:02 +0800

    add some images and change buttonTapped method

--- SWMemberSelector/ViewController.m
+++ SWMemberSelector/ViewController.m
@@ -25,12 +25,22 @@
 
 - (IBAction)buttonTapped:(id)sender {
     
-    NSArray *members = [NSArray array];
+    NSArray *avatars = @[@"avatar1",@"avatar2",@"avatar3",@"avatar4",@"avatar5",@"avatar6"];
+    NSArray *nameArr = @[@"张三三",@"李思思",@"王五五",@"周溜溜",@"萌萌哒",@"Alton"];
+    NSMutableArray *members = [NSMutableArray new];
+    [avatars enumerateObjectsUsingBlock:^(NSString *avatar, NSUInteger idx, BOOL * _Nonnull stop) {
+        SWMember *m = [[SWMember alloc] initWithNickname:nameArr[idx] uid:[NSString stringWithFormat:@"%ld",idx] headerImage:[UIImage imageNamed:avatar] status:SWMemberSelectorStatusUnSelect];
+        [members addObject:m];
+    }];
+    
+    
     SWMemberSelectorController *memberSelectorController = [[SWMemberSelectorController alloc] initWithMembers:members];
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:memberSelectorController];
     [[UINavigationBar appearance] setBarTintColor:[UIColor orangeColor]];
     [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
     [self presentViewController:nav animated:YES completion:nil];
+    
+    
     
 }
 
